820929122152830650ad90954ce36a96eb5d1b19
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-04-20 20:31:47 -0300

    criar classe Preferidas

--- src/br/com/alura/midia/Preferidas.java
+++ src/br/com/alura/midia/Preferidas.java
@@ -1,4 +1,11 @@
 package br.com.alura.midia;
 
 public class Preferidas {
+
+    public void inclui(Audio audio) {
+        if (audio.getClassificacao() > 8)
+            System.out.println(audio.getTitulo() + ": Todo mundo ama!");
+        if (audio.getClassificacao() <= 8)
+            System.out.println(audio.getTitulo() + ": Muitos gostam ");
+    }
 }
